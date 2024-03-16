67ec48efdb7c7671fdff41726404ee398d1d613e
jd11111 <jannik.daun@outlook.de>
2024-03-16 11:32:45 +0100

    modified:   app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -8,8 +8,7 @@ import qualified Data.STRef
 import qualified Control.Monad
 
 
-test :: UArray Int Float; test = listArray (0,4) (repeat 2.0) 
-
+test :: UArray Int Float; test = listArray (0,3) (repeat 2.0) 
 
 matMul :: UArray Int Float -> Int -> Int -> UArray Int Float -> Int -> Int -> UArray Int Float
 matMul a ra ca b rb cb =let{
@@ -19,18 +18,22 @@ matMul a ra ca b rb cb =let{
     getB i j = b!(i*cb + j);
     }
     in Control.Monad.ST.runST $ do{
-    arr <- Data.Array.ST.newArray (0,ra*cb-1) 0.0 :: Control.Monad.ST.ST s (Data.Array.ST.STUArray s Int Float);           
-    
-    let z = Control.Monad.ST.runST $ do{
-        acc <- Data.STRef.newSTRef 0.0;
-        Control.Monad.forM_ [0..(ca-1)] $ \k -> do{Data.STRef.modifySTRef acc (+ getA 0 k)};
-        Data.STRef.readSTRef acc
-    } in
-    Data.Array.ST.writeArray arr 1 z;
+    arr <- Data.Array.ST.newArray (0,ra*cb-1) 0.0 :: Control.Monad.ST.ST s (Data.Array.ST.STUArray s Int Float);
+    Control.Monad.forM_ [0..(ra-1)] $ \i -> do{
+        Control.Monad.forM_ [0..(cb-1)] $ \j -> do{
+            let z = Control.Monad.ST.runST $ do{
+            acc <- Data.STRef.newSTRef 0.0;
+            Control.Monad.forM_ [0..(ca-1)] $ \k -> do{Data.STRef.modifySTRef acc (+ (getA i k + getB k j))};
+            Data.STRef.readSTRef acc
+            } in
+            Data.Array.ST.writeArray arr (i+ra*j) z;
+        };
+    };
     Data.Array.ST.freeze arr;
     }
 
 main :: IO ()
 main =do{
+print $ test;
 print $ matMul test 2 2 test 2 2;
 }
