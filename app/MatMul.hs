2db48c95abb32acb33932680da5a74e3e55f7d29
jd11111 <jannik.daun@outlook.de>
2024-04-04 11:40:49 +0200

    update

--- app/MatMul.hs
+++ app/MatMul.hs
@@ -8,33 +8,33 @@ import qualified Data.Array.Base
 import qualified Data.Array.ST
 import qualified Data.Array.Unboxed
 
--- import qualified Data.STRef
-
 -- define custom matrix type: int_data are the entries of the matrix stored in row major order, rows and cols are obvious
 data Matrix = GenMat {int_data :: Data.Array.Unboxed.UArray Int Float, rows :: Int, cols :: Int} deriving (Show)
 
 matMul :: Matrix -> Matrix -> Matrix
 matMul x y =
-  let a = int_data x
-      ra = rows x
-      ca = cols x
-      b = int_data y
-      rb = rows y
-      cb = cols y
-   in let {-# INLINE getA #-}
-          getA :: Int -> Int -> Float
-          getA i j = Data.Array.Base.unsafeAt a (i * ca + j) -- access the (i,j) entry of a from the row major storage
-          {-# INLINE getB #-}
-          getB :: Int -> Int -> Float
-          getB i j = Data.Array.Base.unsafeAt b (i * cb + j) -- access the (i,j) entry of b from the row major storage
-       in let !z = Data.Array.ST.runSTUArray $ do
-                arr <- Data.Array.ST.newArray (0, ra * cb - 1) 0.0 :: Control.Monad.ST.ST s (Data.Array.ST.STUArray s Int Float) -- zero initialised output array
-                Control.Monad.forM_ [0 .. (ra - 1)] $ \i -> do
+  let{a = int_data x;
+      ra = rows x;
+      ca = cols x;
+      b = int_data y;
+      rb = rows y;
+      cb = cols y;
+      {-# INLINE getA #-};
+      getA :: Int -> Int -> Float;
+      getA i j = Data.Array.Base.unsafeAt a (i * ca + j); -- access the (i,j) entry of a from the row major storage
+      {-# INLINE getB #-};
+      getB :: Int -> Int -> Float;
+      getB i j = Data.Array.Base.unsafeAt b (i * cb + j); -- access the (i,j) entry of b from the row major storage
+      !z = Data.Array.ST.runSTUArray $ do{
+                arr <- Data.Array.ST.newArray (0, ra * cb - 1) 0.0 :: Control.Monad.ST.ST s (Data.Array.ST.STUArray s Int Float); -- zero initialised output array
+                Control.Monad.forM_ [0 .. (ra - 1)] $ \i -> do{
                   -- loop over all rows of a
                   Control.Monad.forM_ [0 .. (cb - 1)] $ \j -> do{
                     -- loop over all columns of b
                     let inner k !var | ca <= k = var | otherwise = inner (k + 1) (var + getA i k * getB k j)
-                    in Data.Array.Base.unsafeWrite arr (i * cb + j) (inner 0 0.0);}
-                return arr
-           in GenMat {int_data = z, rows = ra, cols = cb}
+                    in Data.Array.Base.unsafeWrite arr (i * cb + j) (inner 0 0.0);
+                  };
+                };
+              return arr};
+      } in GenMat {int_data = z, rows = ra, cols = cb}
 
