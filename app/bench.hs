e3c3dee53213765424f7658fd9e77ec3d80ccd3f
jd11111 <jannik.daun@outlook.de>
2024-04-07 14:36:53 +0200

    added complex support

--- app/bench.hs
+++ app/bench.hs
@@ -4,12 +4,14 @@ import Data.Array.Unboxed
 import Data.Time.Clock
 import MatMul
 import System.Random
+import Data.Complex
 
 n :: Int
 n = 500
 
-m = n ^ 2
+m = n ^ 2 ::Int
 
+main :: IO()
 main = do
   gen <- newStdGen
   print "matrix size (square):"
@@ -22,9 +24,8 @@ main = do
   start <- getCurrentTime
   let !testc = matMul mata matb;
   end <- getCurrentTime
-  print "floats:"
-  putStrLn $ "matmul " ++ show (diffUTCTime end start)
-  print $ realTrace (realScaMul 5.0 mata);
+  putStrLn $ "matmul floats:" ++ show (diffUTCTime end start)
+  print $ realTrace (scaMul 5.0 mata);
   let ns = randoms gen :: [Double]
   let !testad = listArray (0, m - 1) (take m ns) :: UArray Int Double
   let !testbd = listArray (0, m - 1) (take m ns) :: UArray Int Double
@@ -33,20 +34,18 @@ main = do
   start <- getCurrentTime
   let !testc = matMul matad matbd;
   end <- getCurrentTime
-  print "doubles:"
-  putStrLn $ "matmul " ++ show (diffUTCTime end start)
-  print $ realTrace (realScaMul 5.0 matad);
-  start <- getCurrentTime
-  let !testc = matMul matad matbd;
-  end <- getCurrentTime
+  putStrLn $ "matmul doubles:" ++ show (diffUTCTime end start)
+  print $ realTrace (scaMul 5.0 matad);
   let ns = randoms gen :: [Float]
   let !testca = listArray (0, 2*m - 1) (take (2*m) ns) :: UArray Int Float
   let !testcb = listArray (0, 2*m - 1) (take (2*m) ns) :: UArray Int Float
-  let !matca = GenCoMat{int_cdata = testca, c_cols = n, c_rows = n};
-  let !matcb = GenCoMat{int_cdata = testcb, c_cols = n, c_rows = n};
+  let !matca = mkComplexMat testca n n;
+  let !matcb = mkComplexMat testcb n n;
   start <- getCurrentTime
-  let !testc = complexMatMul matca matcb;
-  end <- getCurrentTime
-  putStrLn $ "matmul complex" ++ show (diffUTCTime end start)
+  let !testc = matMul matca matcb;
+  end <- getCurrentTime;
+  putStrLn $ "matmul complex: " ++ show (diffUTCTime end start);
+  print $ matTrace (scaMul 5.0 testc);
+  print $ matTrace (scaMul (5.0 :+ 2.32) testc);
 
 
--- lib/MatMul.hs
+++ lib/MatMul.hs
@@ -20,14 +20,18 @@ class ( Show a, RealFloat a) => MyNums a where
   myListArray :: (Int, Int) -> [a] -> Data.Array.Unboxed.UArray Int a;
 
 instance MyNums Float where
+  {-# INLINE myUnsafeAt #-}
   myUnsafeAt =Data.Array.Base.unsafeAt :: UArray Int Float -> Int -> Float;
   myNewArray = Data.Array.ST.newArray :: (Int,Int) -> Float -> Control.Monad.ST.ST s (Data.Array.ST.STUArray s Int Float);
+  {-# INLINE myUnsafeWrite #-}
   myUnsafeWrite = Data.Array.Base.unsafeWrite :: Data.Array.ST.STUArray s Int Float -> Int -> Float -> Control.Monad.ST.ST s ();
   myListArray = listArray;
 
 instance MyNums Double where
+  {-# INLINE myUnsafeAt #-}
   myUnsafeAt =Data.Array.Base.unsafeAt :: UArray Int Double -> Int -> Double;
   myNewArray = Data.Array.ST.newArray :: (Int,Int) -> Double -> Control.Monad.ST.ST s (Data.Array.ST.STUArray s Int Double);
+  {-# INLINE myUnsafeWrite #-}
   myUnsafeWrite = Data.Array.Base.unsafeWrite :: Data.Array.ST.STUArray s Int Double -> Int -> Double -> Control.Monad.ST.ST s ();
   myListArray = listArray;
 
@@ -49,12 +53,12 @@ instance ProtoMat MyMatrix Float where
 
 instance RealMat MyMatrix Float where
   int_data  = intf_data
-  mkRealMat a r c = GenFMat{intf_data = a, f_rows = r, f_cols = c}
+  mkRealMat a r c | r*c -1 == snd (Data.Array.Base.bounds a) = GenFMat{intf_data = a, f_rows = r, f_cols = c}| otherwise = undefined
 
 class Matrix m a where
   matMul :: m a -> m a -> m a
-  scaMul :: a -> m a -> m a 
-  matTrace :: m a -> a 
+  scaMul :: a -> m a -> m a
+  matTrace :: m a -> a
 
 instance Matrix MyMatrix Float where
   matMul = realMatMul
@@ -74,9 +78,46 @@ instance ProtoMat MyMatrix Double where
 
 instance RealMat MyMatrix Double where
   int_data = intd_data
-  mkRealMat a r c = GenDMat{intd_data = a, d_rows = r, d_cols = c}
+  mkRealMat a r c | r*c -1 == snd (Data.Array.Base.bounds a) && fst (Data.Array.Base.bounds a) ==0= GenDMat{intd_data = a, d_rows = r, d_cols = c}| otherwise = undefined
+
+{-# INLINE realMatMul #-}
+realMatMul :: RealMat m a => m a -> m a -> m a
+realMatMul x y =
+  let{a = int_data x;
+      ra = rows x;
+      ca = cols x;
+      b = int_data y;
+      rb = rows y;
+      cb = cols y;
+       {-# INLINE getA #-};
+      getA i j = myUnsafeAt a (i * ca + j); -- access the (i,j) entry of a from the row major storage
+      {-# INLINE getB #-};
+      getB i j = myUnsafeAt b (i * cb + j); -- access the (i,j) entry of b from the row major storage
+      !z = Data.Array.ST.runSTUArray $ do{
+                arr <- myNewArray (0, ra * cb - 1) 0.0;
+                Control.Monad.forM_ [0 .. (ra - 1)] $ \i -> do{
+                  Control.Monad.forM_ [0 .. (cb - 1)] $ \j -> do{
+                    let inner k !var | ca <= k = var | otherwise = inner (k + 1) (var + getA i k * getB k j)
+                    in myUnsafeWrite arr (i * cb + j) (inner 0 0.0);
+                  };
+                };
+              return arr};
+      } in if ca == rb then mkRealMat z ra cb else undefined
+
+realTrace :: RealMat m a => m a -> a
+realTrace x =
+  let {a = int_data x;
+      ca = cols x;
+      ra = rows x;}
+   in if ca == ra then sum [ myUnsafeAt a (i * ca + i) | i <- range (0, ca - 1)] else undefined
+
+realScaMul :: RealMat m a => a -> m a -> m a
+realScaMul alpha x = let a = int_data x; ra = rows x; ca = cols x; z = myListArray (0, ra*ca-1) [ alpha * myUnsafeAt a (i*ca +j) | i <- range (0, ra-1), j <- range(0,ca-1)] in mkRealMat z ra ca
 
 data instance (MyNums a) => MyMatrix (Complex a)  = GenCoMat {int_cdata :: Data.Array.Unboxed.UArray Int a, c_rows :: Int, c_cols :: Int}
+
+mkComplexMat:: (IArray UArray a, MyNums a) => UArray Int a -> Int -> Int -> MyMatrix (Complex a)
+mkComplexMat a r c | 2*r*c -1 == snd (Data.Array.Base.bounds a) && fst (Data.Array.Base.bounds a) ==0= GenCoMat{int_cdata = a, c_rows = r, c_cols = c}| otherwise = undefined
 
 instance (MyNums a) => ProtoMat MyMatrix (Complex a) where
   cols = c_cols
@@ -110,40 +151,36 @@ complexMatMul x y =
               return arr};
       } in if ca == rb then GenCoMat{int_cdata = z, c_rows = ra, c_cols = cb} else undefined
 
-{-# INLINE realMatMul #-} --change this back to low level implementation ?
-realMatMul :: RealMat m a => m a -> m a -> m a
-realMatMul x y =
-  let{a = int_data x;
+
+complexTrace :: (MyNums a) => MyMatrix (Complex a) -> Complex a
+complexTrace x =
+  let {a = int_cdata x;
+      ca = cols x;
+      ra = rows x;}
+   in if ca == ra then sum [ myUnsafeAt a (2*(i * ca + i)) | i <- range (0, ca - 1)] :+ sum [ myUnsafeAt a (2*(i * ca + i)+1) | i <- range (0, ca - 1)] else undefined
+
+{-# INLINE complexScaMul #-}
+complexScaMul :: (MyNums a) => Complex a -> MyMatrix (Complex a) -> MyMatrix (Complex a)
+complexScaMul alpha x =
+  let{a = int_cdata x;
       ra = rows x;
       ca = cols x;
-      b = int_data y;
-      rb = rows y;
-      cb = cols y;
-       {-# INLINE getA #-};
-      --getA :: MyNums t =>  Int -> Int -> t;
-      getA i j = myUnsafeAt a (i * ca + j); -- access the (i,j) entry of a from the row major storage
-      {-# INLINE getB #-};
-      --getB :: Int -> Int -> t;
-      --getB i j = myUnsafeAt b (i * cb + j); -- access the (i,j) entry of b from the row major storage
-      getB i j = myUnsafeAt b (i * cb + j); -- access the (i,j) entry of b from the row major storage --revert this!
+      {-# INLINE getReA #-};
+      getReA i j = myUnsafeAt a (2*(i * ca + j));
+      {-# INLINE getImA #-};
+      getImA i j = myUnsafeAt a (2*(i * ca + j)+1);
       !z = Data.Array.ST.runSTUArray $ do{
-                arr <- myNewArray (0, ra * cb - 1) 0.0;-- :: Control.Monad.ST.ST s (Data.Array.ST.STUArray s Int Float); -- zero initialised output array
+                arr <- myNewArray (0, 2*ra * ca - 1) 0.0;
                 Control.Monad.forM_ [0 .. (ra - 1)] $ \i -> do{
-                  -- loop over all rows of a
-                  Control.Monad.forM_ [0 .. (cb - 1)] $ \j -> do{
-                    -- loop over all columns of b
-                    let inner k !var | ca <= k = var | otherwise = inner (k + 1) (var + getA i k * getB k j)
-                    in myUnsafeWrite arr (i * cb + j) (inner 0 0.0);
+                  Control.Monad.forM_ [0 .. (ca - 1)] $ \j -> do{
+                    let {var = alpha * (getReA i j :+ getImA i j)}
+                    in do{ myUnsafeWrite arr (2*(i * ca + j)) (realPart var); myUnsafeWrite arr (2*(i * ca + j)+1) (imagPart var);}
                   };
                 };
               return arr};
-      } in if ca == rb then mkRealMat z ra cb else undefined
+      } in GenCoMat{int_cdata = z, c_rows = ra, c_cols = ca}
 
-realTrace :: RealMat m a => m a -> a
-realTrace x =
-  let a = int_data x
-      ca = cols x
-   in sum [ myUnsafeAt a (i * ca + i) | i <- range (0, ca - 1)]
-
-realScaMul :: RealMat m a => a -> m a -> m a
-realScaMul alpha x = let a = int_data x; ra = rows x; ca = cols x; z = myListArray (0, ra*ca-1) [ alpha * myUnsafeAt a (i*ca +j) | i <- range (0, ra-1), j <- range(0,ca-1)] in mkRealMat z ra ca
+instance (MyNums a) => Matrix MyMatrix (Complex a) where 
+  matMul = complexMatMul
+  scaMul = complexScaMul
+  matTrace = complexTrace
